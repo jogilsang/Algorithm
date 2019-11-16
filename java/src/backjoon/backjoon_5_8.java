cc72a1f46e6c513954c99c1813a8531c837310f9
jogilsang <jogilsang@gmail.com>
2019-11-17 03:08:08 +0900

    backjoon step7_1 added

--- java/src/backjoon/backjoon_5_8.java
+++ java/src/backjoon/backjoon_5_8.java
@@ -1,54 +1,10 @@
 package backjoon;
 
-import java.util.Scanner;
-
 public class backjoon_5_8 {
 
+    // 3분 10초부터시작
+
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine(); // int와 String 사이 공백에러발생, 제거요망
-
-        // 입력값 index에 맞게 점수를 집어넣음
-
-        // 한 줄씩 평균을 넘는 학생들의 비율
-        float[] points = new float[n];
-
-        // 입력값
-        String[] arr = new String[n];
-
-
-
-        for (int i = 0; i < n; i++) {
-            // 문자열을 입력받음 (OOOOXXXXOOO)
-            arr[i] = sc.nextLine();
-
-            // 포인트와 포인트합산값을 선언
-            int point = 0;
-            int sum = 0;
-
-            // O,X를 각각 쪼개서 새롭게 배열을 만듬
-            String[] temp = arr[i].split("");
-            for (int j = 0; j < temp.length; j++) {
-
-                // O를 만나면 point 증가 그리고 점수 추가
-                if (temp[j].equals("O")) {
-                    sum += ++point;
-                }
-                // X를 만나면 0으로 초기화
-                else {
-                    point = 0;
-                }
-            }
-            // 포인트 합산값을 포인트 배열에 넣음
-            points[i] = sum;
-        }
-
-        // 각각의 문자열 계산한 포인트를 출력함
-        for (int point : points) {
-            System.out.println(point);
-        }
 
     }
 
---
+++ java/src/backjoon/backjoon_7_1.java
@@ -0,0 +1,20 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class backjoon_7_1 {
+
+    // 3분 10초부터시작
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        char value = sc.nextLine().charAt(0);
+        int ascii = (int)value;
+
+        System.out.println(ascii);
+
+    }
+
+}
