61322a3771a0b14843655762047e42093e13c431
jogilsang <jogilsang@gmail.com>
2019-11-22 23:08:41 +0900

    added : backjoon step3_9.kt(03:22)

---
+++ kotlin/src/backjoon/kotlin/backjoon_3_9.kt
@@ -0,0 +1,20 @@
+package backjoon.kotlin
+// kotlin
+// 2019-11-22, time : 03:22, solved by gilsang
+fun main(args: Array<String>) {
+
+    var sc:String? = readLine()
+    var n = sc.toString().toInt()
+    var answer:String = ""
+
+    for (i in 1..n) {
+        for(j in 1..i) {
+            answer += "*"
+        }
+        answer += "\n"
+    }
+    println(answer)
+
+}
+
+
