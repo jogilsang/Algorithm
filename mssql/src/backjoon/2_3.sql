a8c35b4c1e3cfb80ff0555e42fc80e764b072a5e
jogilsang <jogilsang@gmail.com>
2020-03-27 04:39:24 +0900

    added : backjoon stop2_3.sql (03:00)

---
+++ mssql/src/backjoon/2_3.sql
@@ -0,0 +1,20 @@
+﻿-- mssql
+-- Q : ﻿2753번
+-- C:\algo\Algorithm\mssql\src\backjoon
+
+DROP PROC main2_3
+
+CREATE PROC main2_3
+	@value INT
+AS
+	IF(((@value / 4) = 1 AND (@value / 100) != 1) OR (@value / 400 = 1)) 
+		BEGIN
+			PRINT '윤년';
+		END
+	ELSE
+		BEGIN
+			PRINT '윤년 아님';
+		END
+GO
+
+EXEC main2_3 1900
