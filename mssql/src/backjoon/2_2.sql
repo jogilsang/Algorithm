5a7225d3a7b52c4db307d794bb758fb9f73d6567
jogilsang <jogilsang@gmail.com>
2020-03-27 04:31:29 +0900

    added : backjoon stop2_2.sql (02:00)

---
+++ mssql/src/backjoon/2_2.sql
@@ -0,0 +1,33 @@
+﻿-- mssql
+-- Q : ﻿9498번
+-- C:\algo\Algorithm\mssql\src\backjoon
+
+DROP PROC main2_2
+
+CREATE PROC main2_2
+	@input INT
+AS
+		IF(@input >= 90 AND @input <= 100)
+			BEGIN
+				PRINT 'A';
+			END
+		ELSE IF(@input >= 80 AND @input < 90)
+			BEGIN
+				PRINT  'B';
+			END
+		ELSE IF(@input >= 70 AND @input < 80)
+			BEGIN
+				PRINT 'C';
+			END
+		ELSE IF(@input >= 60 AND @input < 70)
+			BEGIN
+				PRINT 'D';
+			END
+		ELSE 
+			BEGIN
+				PRINT 'F';
+			END
+			
+GO
+
+EXEC main2_2 80
