17a3f713b5391832045257c914b39beb23824e92
jogilsang <jogilsang@gmail.com>
2020-06-03 05:39:05 +0900

    added : backjoon step2_3(00:00), mssql

---
+++ mssql/src/backjoon/2_3.sql
@@ -0,0 +1,21 @@
+﻿
+-- mssql
+-- Q : ﻿2753번
+-- C:\algo\Algorithm\mssql\src\backjoon
+-- DROP PROC main2_3
+
+CREATE PROC main2_3
+	@input INT
+AS
+	IF((@input / 4 = 1 AND @input / 100 != 1) OR @input / 400 = 1)
+		BEGIN
+			PRINT '1';
+		END
+	ELSE
+		BEGIN
+			PRINT '0';
+		END
+			
+GO
+
+EXEC main2_3 2020
