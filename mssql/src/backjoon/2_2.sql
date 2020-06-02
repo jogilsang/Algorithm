c2d9099bdf2f3978ac2412d918d209d29fb793df
jogilsang <jogilsang@gmail.com>
2020-06-03 05:39:17 +0900

    added : backjoon step2_2(00:00), mssql

---
+++ mssql/src/backjoon/2_2.sql
@@ -0,0 +1,30 @@
+
+--DROP PROC main2_2
+
+CREATE PROC main2_2
+	@input INT
+AS
+		IF(@input >= 90 AND @input <= 100)
+			BEGIN
+				PRINT 'A';
+			END
+		ELSE IF(@input >= 80 AND @input < 90)
+			BEGIN
+				PRINT  'B';
+			END
+		ELSE IF(@input >= 70 AND @input < 80)
+			BEGIN
+				PRINT 'C';
+			END
+		ELSE IF(@input >= 60 AND @input < 70)
+			BEGIN
+				PRINT 'D';
+			END
+		ELSE 
+			BEGIN
+				PRINT 'F';
+			END
+			
+GO
+
+EXEC main2_2 80
