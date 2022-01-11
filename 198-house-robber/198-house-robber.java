d1bbaf626197c0164ca8916a7fd8cc23e13f7ce1
GilSang Jo <jogilsang@naver.com>
2022-01-11 21:36:50 +0900

    Time: 0 ms (100.00%), Space: 36.6 MB (30.97%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -2,6 +2,18 @@ class Solution {
     public int rob(int[] nums) {
         int[] totalRob = new int[nums.length];
         
+        // solution
+        // 1. totalRob[0]~totalRob[2] = 경우의수가 1개
+        // 2. totalRob[3] ~ totalRob[last] = 경우의수가 2개
+        // 3. 점화식
+        
+        // testcase
+        // [0]
+        // [1]
+        // [1,1]
+        // [1,2,3]
+        // [1,2,3,4,5]
+        
         if(nums.length == 1) {
             return nums[0];
         }
@@ -16,8 +28,6 @@ class Solution {
         totalRob[1] = nums[1];
         totalRob[2] = nums[0] + nums[2];
         
-        // index가 홀수인경우, max(item[n-1],item[n-3])+item[n]
-        // index가 짝수인경우, max(item[n-2],item[n-3])+item[n]
         for(int i = 3 ; i < nums.length  ; i++){
           totalRob[i] = Math.max(totalRob[i-2], totalRob[i-3]) + nums[i];
         }
