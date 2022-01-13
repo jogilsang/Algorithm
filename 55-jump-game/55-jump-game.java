826137557d87eaeacfddcb2629685044503ed1b1
GilSang Jo <jogilsang@naver.com>
2022-01-14 05:19:43 +0900

    Time: 329 ms (16.86%), Space: 39.5 MB (92.44%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -6,13 +6,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
         
+        // Solution
+        // 1. nums의 num값만큼 true로 바꿔주기
+        // 2. true면, 더 갈수있기때문에 더표시하기
+        // 3. false면, 최대값까지 왔기때문에 break하기 
         boolean[] dp = new boolean[10001];
         dp[0] = true;
         
         for (int i = 0 ; i < nums.length ; i++) {
             int maxStep = nums[i];
+            // 이동가능하기때문에, 이동해서 이동가능거리 boolean표기
             if(dp[i]) {
                 int j = i+1;
+                // 최대이동거리만큼 true로 표시하기
                 while(maxStep != 0){
                     if(j != nums.length) {
                         dp[j]=true;
@@ -23,10 +29,12 @@ class Solution {
                     j++;
                     maxStep--;
                 }
-            }else {
+            } // 이동가능한 최대값을 이미 넘어섰음, false
+            else {
                 return false;
             }
         }
+        // 배열의 끝에 도달했으므로, true
         return true;
         
     }
